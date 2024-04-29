f9674994d3b7d76c280d17822b0718f9817d641e
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-04-29 17:15:27 -0300

    style: add animation zoomIn

--- tailwind.config.js
+++ tailwind.config.js
@@ -115,11 +115,16 @@ module.exports = {
       },
       animation: {
         float: 'float 3s ease-in-out infinite',
+        zoomIn: 'zoomIn 3s ease-in-out infinite',
       },
       keyframes: {
         float: {
           '0%, 100%': { transform: 'translateY(0)', },
           '50%': { transform: 'translateY(-5%)', },
+        },
+        zoomIn: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.05)' },
         },
       },
       zIndex: {
