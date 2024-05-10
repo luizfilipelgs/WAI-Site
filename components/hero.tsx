73dcd2868950184c9a4bda372592999c4a34d532
pllsg96 <pedroluizlsg@gmail.com>
2024-05-10 15:25:00 -0300

    feat: homepage

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,100 +1,35 @@
-import VideoThumb from '@/public/images/logo.png'
-import ModalVideo from '@/components/modal-video'
+// components/FullWidthBackgrounds.js
+import image1 from '@/public/images/foto-w.1.webp'
+import image2 from '@/public/images/crystal-photo.webp'
 
 export default function Hero() {
   return (
-    <section className="relative">
-      {/* Illustration behind hero content */}
-      <div
-        className="pointer-events-none absolute bottom-0 left-1/2 -z-1 -translate-x-1/2 transform"
-        aria-hidden="true"
-      >
-        <svg
-          width="1360"
-          height="578"
-          viewBox="0 0 1360 578"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <linearGradient
-              x1="50%"
-              y1="0%"
-              x2="50%"
-              y2="100%"
-              id="illustration-01"
-            >
-              <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#illustration-01)" fillRule="evenodd">
-            <circle cx="1232" cy="128" r="128" />
-            <circle cx="155" cy="443" r="64" />
-          </g>
-        </svg>
-      </div>
-
-      <div className="mx-auto max-w-6xl px-4 sm:px-8">
-        {/* Hero content */}
-        <div className="pb-12 pt-32 md:pb-20 md:pt-40">
-          {/* Section header */}
-          <div className="pb-12 text-center md:pb-16">
-            <h1
-              className="mb-4 text-5xl font-extrabold leading-tighter tracking-tighter md:text-6xl"
-              data-aos="zoom-y-out"
-            >
-              Make your website{' '}
-              <span className="bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent">
-                wonderful
-              </span>
-            </h1>
-            <div className="mx-auto max-w-3xl">
-              <p
-                className="mb-8 text-xl text-gray-600"
-                data-aos="zoom-y-out"
-                data-aos-delay="150"
-              >
-                Our landing page template works on all devices, so you only have
-                to set it up once, and get beautiful results forever.
-              </p>
-              <div
-                className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center"
-                data-aos="zoom-y-out"
-                data-aos-delay="300"
-              >
-                <div>
-                  <a
-                    className="btn mb-4 w-full bg-blue-600 text-white hover:bg-blue-700 sm:mb-0 sm:w-auto"
-                    href="#0"
-                  >
-                    Start free trial
-                  </a>
-                </div>
-                <div>
-                  <a
-                    className="btn w-full bg-gray-900 text-white hover:bg-gray-800 sm:ml-4 sm:w-auto"
-                    href="#0"
-                  >
-                    Learn more
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Hero image */}
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={768}
-            thumbHeight={432}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080}
-          />
+    <div className="w-full overflow-auto">
+      <div className="flex h-screen w-full items-center justify-end bg-cover bg-center"
+        style={{ backgroundImage: `url(${image1.src})` }}>
+        <div className="w-1/4 p-0 m-0 text-left" data-aos="fade-right">
+          <h1 className="text-5xl font-semibold">Oracle [W]</h1>
+          <h2 className="text-2xl">A água encontra a I.A.</h2>
+          <a href="#" className="text-2xl font-bold text-blue-500 hover:text-blue-800">Saiba mais</a>
+      <div className="neon-line"></div>
         </div>
       </div>
-    </section>
+    <div className="neon-line"></div>
+      <div
+        className="flex h-screen w-full items-center justify-center bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${image2.src})`,
+          backgroundPosition: '70%',
+        }}
+      >
+        <div className="ml-20 flex flex-col w-full items-left justify-start p-4 text-center text-black">
+          <h1 className="text-5xl font-medium w-1/4 px-4 text-left" data-aos="fade-left">[W].AI</h1>
+          <p className="text-2xl w-1/5 px-4 text-left" data-aos="fade-left">Água alimetica.</p>
+          <p className="text-2xl w-1/6 px-4 text-left" data-aos="fade-left">Saúde e beleza para seu corpo, mente e alma.</p> 
+          <a href="#" className="text-2xl w-1/6 px-4 text-left text-red-500 hover:text-red-800">Saiba mais</a>
+        </div>
+      </div>
+      <div className="neon-line"></div>
+    </div>
   )
 }
