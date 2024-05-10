e9ceb57cc03ca79d54a411fb52c7523f1e58012d
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-05-10 15:32:30 -0300

    refactor: pequenos ajustes, identação,

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,28 +5,48 @@ import image2 from '@/public/images/crystal-photo.webp'
 export default function Hero() {
   return (
     <div className="w-full overflow-auto">
-      <div className="flex h-screen w-full items-center justify-end bg-cover bg-center"
-        style={{ backgroundImage: `url(${image1.src})` }}>
-        <div className="w-1/4 p-0 m-0 text-left" data-aos="fade-right">
+      <div
+        className="flex h-screen w-full items-center justify-end bg-cover bg-center"
+        style={{ backgroundImage: `url(${image1.src})` }}
+      >
+        <div className="m-0 w-1/4 p-0 text-left" data-aos="fade-right">
           <h1 className="text-5xl font-semibold">Oracle [W]</h1>
           <h2 className="text-2xl">A água encontra a I.A.</h2>
-          <a href="#" className="text-2xl font-bold text-blue-500 hover:text-blue-800">Saiba mais</a>
-      <div className="neon-line"></div>
+          <a
+            href="#"
+            className="text-2xl font-bold text-blue-500 hover:text-blue-800"
+          >
+            Saiba mais
+          </a>
         </div>
       </div>
-    <div className="neon-line"></div>
+      <div className="neon-line"></div>
       <div
-        className="flex h-screen w-full items-center justify-center bg-cover bg-center"
+        className="flex w-full items-center justify-center bg-cover bg-center"
         style={{
           backgroundImage: `url(${image2.src})`,
           backgroundPosition: '70%',
         }}
       >
-        <div className="ml-20 flex flex-col w-full items-left justify-start p-4 text-center text-black">
-          <h1 className="text-5xl font-medium w-1/4 px-4 text-left" data-aos="fade-left">[W].AI</h1>
-          <p className="text-2xl w-1/5 px-4 text-left" data-aos="fade-left">Água alimetica.</p>
-          <p className="text-2xl w-1/6 px-4 text-left" data-aos="fade-left">Saúde e beleza para seu corpo, mente e alma.</p> 
-          <a href="#" className="text-2xl w-1/6 px-4 text-left text-red-500 hover:text-red-800">Saiba mais</a>
+        <div className="items-left ml-20 flex w-full flex-col justify-start p-4 text-center text-black">
+          <h1
+            className="w-1/4 px-4 text-left text-5xl font-medium"
+            data-aos="fade-left"
+          >
+            [W].AI
+          </h1>
+          <p className="w-1/5 px-4 text-left text-2xl" data-aos="fade-left">
+            Água alimetica.
+          </p>
+          <p className="w-1/6 px-4 text-left text-2xl" data-aos="fade-left">
+            Saúde e beleza para seu corpo, mente e alma.
+          </p>
+          <a
+            href="#"
+            className="w-1/6 px-4 text-left text-2xl font-bold text-red-500 hover:text-red-800"
+          >
+            Saiba mais
+          </a>
         </div>
       </div>
       <div className="neon-line"></div>
