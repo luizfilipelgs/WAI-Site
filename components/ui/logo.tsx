a12428b6e3d12cab2990f8e17761487816094ef1
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-05-20 09:33:22 -0300

    fix: altera import logo para githubpages

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import logoImg from '@/public/images/logo.svg'
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="WAI">
-      <Image src="/images/logo.svg" alt="WAI logo" width={35} height={35} />
+      <Image src={logoImg} alt="WAI logo" width={35} height={35} />
     </Link>
   )
 }
