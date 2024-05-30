5489d8cf2201ec250e0a486c10110aac13f14a59
pllsg96 <pedroluizlsg@gmail.com>
2024-05-30 17:07:22 -0300

    fix: hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
   return (
     <div className="w-full overflow-auto">
       <div
-        className="flex w-full items-center justify-end bg-cover bg-center"
+        className="flex w-full m-0 items-center justify-end bg-cover bg-center"
         style={{
           backgroundImage: `url(${image1.src})`,
           backgroundSize: 'contain',
@@ -16,10 +16,10 @@ export function Hero() {
         }}
       >
         <div className="m-0 w-1/4 p-0 text-left" data-aos="fade-right">
-          <h1 className="text-5xl font-semibold">Oracle [W]</h1>
-          <h2 className="text-2xl">A água encontra a I.A.</h2>
+          <h1 className="text-4xl font-semibold">Oracle [W]</h1>
+          <h2 className="text-xl">A água encontra a I.A.</h2>
           <Link href="#">
-            <p className="cursor-pointer text-2xl font-bold text-blue-500 hover:text-blue-800">
+            <p className="cursor-pointer text-l font-bold text-blue-500 hover:text-blue-800">
               Saiba mais
             </p>
           </Link>
@@ -35,24 +35,26 @@ export function Hero() {
           height: '60vh',
         }}
       >
-        <div className="items-left ml-20 flex w-full flex-col justify-start p-4 text-center text-black">
+        <div className="items-left ml-10 flex w-full flex-col justify-start p-4 text-center text-black">
           <h1
-            className="w-1/4 px-4 text-left text-5xl font-medium"
+            className="px-4 text-left text-4xl font-medium"
             data-aos="fade-left"
           >
             [W].AI
           </h1>
-          <p className="w-1/5 px-4 text-left text-2xl" data-aos="fade-left">
-            Água alimetica.
-          </p>
-          <p className="w-1/6 px-4 text-left text-2xl" data-aos="fade-left">
-            Saúde e beleza para seu corpo, mente e alma.
-          </p>
-          <Link href="#">
-            <p className="w-1/6 cursor-pointer px-4 text-left text-2xl font-bold text-red-500 hover:text-red-800">
-              Saiba mais
+          <div className="w-1/4">  
+            <p className="px-4 text-left text-xl" data-aos="fade-left">
+              Água alimetica.
             </p>
-          </Link>
+            <p className="px-4 text-left text-xl" data-aos="fade-left">
+              Saúde e beleza para seu corpo, mente e alma.
+            </p>
+            <Link href="#">
+              <p className="cursor-pointer px-4 text-left text-xl font-bold text-red-500 hover:text-red-800">
+                Saiba mais
+              </p>
+            </Link>
+          </div>
         </div>
       </div>
       <div className="neon-line"></div>
